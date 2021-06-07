20994c6ca4aacd79fe60f6854749fd473a9eaebc
Swapnil Sankla <swapnil.sankla@gmail.com>
2021-06-07 21:38:39 +0530

    Bump header cart button after adding/removing item to/from cart

--- src/Layout/HeaderCartButton.js
+++ src/Layout/HeaderCartButton.js
@@ -1,22 +1,38 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../Store/cart-context";
 import styles from "./HeaderCartButton.module.css";
 
 const HeaderCardButton = (props) => {
   const cartContext = useContext(CartContext);
+  const [highlightButton, setHighlightButton] = useState(false);
+  const { numberOfItemsInCart } = cartContext;
+  useEffect(() => {
+    if (numberOfItemsInCart > 0) {
+      setHighlightButton(true);
+    }
+    const timeout = setTimeout(() => {
+      setHighlightButton(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [numberOfItemsInCart]);
 
   const buttonClickHandler = () => {
     props.cartButtonClicked();
   };
 
+  const buttonClass = `${styles.button} ${highlightButton && styles.bump}`;
+
   return (
-    <button className={styles.button} onClick={buttonClickHandler}>
+    <button className={buttonClass} onClick={buttonClickHandler}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
       <span>Cart</span>
-      <span className={styles.badge}>{cartContext.numberOfItemsInCart}</span>
+      <span className={styles.badge}>{numberOfItemsInCart}</span>
     </button>
   );
 };
